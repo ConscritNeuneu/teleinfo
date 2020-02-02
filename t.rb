bc0ce8b5ef90795fd223277117fbadc3ccd414fd
Quentin Godfroy <quentin@godfroy.eu>
2020-02-02 14:37:24 +0000

    Add mutex and report

--- t.rb
+++ t.rb
@@ -7,6 +7,7 @@ GENERAL_METER = "/dev/ttyAMA0"
 SPECIAL_METER = "/dev/ttyAMA1"
 SPECIAL_METER_ID = 1
 METER_DATABASE = "/home/quentin/index_reports.sqlite3"
+REPORT_FILE = '/tmp/report.txt'
 
 def validate_historic(sub_group)
   data = sub_group[0..-3]
@@ -114,6 +115,17 @@ def adjust_index(split, index_name, value)
   split[index_name] += value
 end
 
+def write_report(report_file, meter_id, meter_split)
+  File.open(report_file, 'wb') do |f|
+    f.write("Report for meter_id #{meter_id}\n\n")
+    f.write(
+      meter_split.map do |index, consumption|
+        "#{index}\t#{(consumption.to_f/100).round / 10} kWh"
+      end.join("\n")
+    )
+  end
+end
+
 INDEXES = {
   "HCJB" => "blue_off_peak",
   "HCJW" => "white_off_peak",
@@ -126,6 +138,7 @@ UNKNOWN_INDEX = "unknown"
 
 db = setup_database(METER_DATABASE)
 
+mutex = Mutex.new
 general_meter_current_index_name = UNKNOWN_INDEX
 general_meter_current_index_time = Time.now
 special_meter_index_sync = false
@@ -134,43 +147,58 @@ record_incident(db, "read indexes from database for meter #{SPECIAL_METER_ID} wi
 
 Thread.new do
   read_meter_info(GENERAL_METER) do |meter_info|
-    ptec = meter_info["PTEC"]
-    if ptec && INDEXES[ptec]
-      if INDEXES[ptec] != general_meter_current_index_name
-        record_incident("Setting general meter index from #{general_meter_current_index_name} to #{INDEXES[ptec]}")
+    mutex.synchronize do
+      ptec = meter_info["PTEC"]
+      if ptec && INDEXES[ptec]
+        if INDEXES[ptec] != general_meter_current_index_name
+          record_incident("Setting general meter index from #{general_meter_current_index_name} to #{INDEXES[ptec]}")
+        end
+        general_meter_current_index_name = INDEXES[ptec]
+        general_meter_current_index_time = Time.now
       end
-      general_meter_current_index_name = INDEXES[ptec]
-      general_meter_current_index_time = Time.now
     end
   end
 end
 
 Thread.new do
   read_meter_info(SPECIAL_METER) do |meter_info|
-    old_sum = special_meter_index_split.sum { |_, idx| idx }
-    new_sum = sum_indexes(meter_info)
-    if new_sum
-      delta = new_sum - old_sum
-      if !special_meter_index_sync
-        if delta != 0
-          adjust_index(special_meter_index_split, UNKNOWN_INDEX, delta)
-          save_indexes(db, SPECIAL_METER_ID, special_meter_index_split)
-          record_incident(db, "adjust unknown index from meter #{SPECIAL_METER_ID} by #{delta}Wh")
-        end
-        special_meter_index_sync = true
-      else
-        if delta > 0
-          adjust_index(special_meter_index_split, general_meter_current_index_name, delta)
-          if general_meter_current_index_name == UNKNOWN_INDEX
-            record_incident(db, "ventilate #{delta}Wh of meter #{SPECIAL_METER_ID} into the unknown index")
+    mutex.synchronize do
+      old_sum = special_meter_index_split.sum { |_, idx| idx }
+      new_sum = sum_indexes(meter_info)
+      if new_sum
+        delta = new_sum - old_sum
+        if !special_meter_index_sync
+          if delta != 0
+            adjust_index(special_meter_index_split, UNKNOWN_INDEX, delta)
+            save_indexes(db, SPECIAL_METER_ID, special_meter_index_split)
+            record_incident(db, "adjust unknown index from meter #{SPECIAL_METER_ID} by #{delta}Wh")
           end
-        elsif delta < 0
-          adjust_index(special_meter_index_split, UNKNOWN_INDEX, delta)
-          save_indexes(db, SPECIAL_METER_ID, special_meter_index_split)
-          record_incident(db, "negative consumption of #{SPECIAL_METER_ID} of #{delta}Wh")
+          special_meter_index_sync = true
+        else
+          if delta > 0
+            adjust_index(special_meter_index_split, general_meter_current_index_name, delta)
+            if general_meter_current_index_name == UNKNOWN_INDEX
+              record_incident(db, "ventilate #{delta}Wh of meter #{SPECIAL_METER_ID} into the unknown index")
+            end
+          elsif delta < 0
+            adjust_index(special_meter_index_split, UNKNOWN_INDEX, delta)
+            save_indexes(db, SPECIAL_METER_ID, special_meter_index_split)
+            record_incident(db, "negative consumption of #{SPECIAL_METER_ID} of #{delta}Wh")
+          end
         end
       end
     end
+  end
+end
+
+Thread.new do
+  loop do
+    sleep 10
+    split = nil
+    mutex.synchronize do
+      split = special_meter_index_split.dup
+    end
+    write_report(REPORT_FILE, SPECIAL_METER_ID, split)
   end
 end
 
@@ -181,11 +209,13 @@ end
 
 loop do
   sleep(1800)
-  save_indexes(db, SPECIAL_METER_ID, special_meter_index_split)
-  if (Time.now - general_meter_current_index_time) >= 1800
-    general_meter_current_index_name = UNKNOWN_INDEX
-    general_meter_current_index_time = Time.now
-    record_incident(db, "Reverting general meter index to unknown. Last update was #{general_meter_current_index_update}.")
+  mutex.synchronize do
+    save_indexes(db, SPECIAL_METER_ID, special_meter_index_split)
+    if (Time.now - general_meter_current_index_time) >= 1800
+      general_meter_current_index_name = UNKNOWN_INDEX
+      general_meter_current_index_time = Time.now
+      record_incident(db, "Reverting general meter index to unknown. Last update was #{general_meter_current_index_update}.")
+    end
   end
 end
 #trame = "\x02\nADCO 811775412275 I\r\nOPTARIF HC.. <\r\nISOUSC 45 ?\r\nHCHC 004070290 \\\r\nHCHP 006438891 :\r\nPTEC HP..  \r\nIINST 004 [\r\nIMAX 090 H\r\nPAPP 01090 +\r\nHHPHC A ,\r\nMOTDETAT 000000 B\r\x03"
